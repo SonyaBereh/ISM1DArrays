29cdb4f15666f6a40a97f18dd1201ed7d43cba9b
SonyaBereh <chernovskaya.sophia@yandex.ua>
2016-09-28 23:00:13 +0300

    ISM1DArrays

---
+++ Arrays2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Введіть N: ");
+            int N = int.Parse(Console.ReadLine());
+            int[] arr = new int[N];
+            Random rnd = new Random();
+            int rez = 1, rez1 = 0, rez2 = 0, rez3 = 1, min = 100, min1 = 100, max = -100, l = 0, g = 0, k = 0, n = 0, a = 0, m = 0, b = 0, c = 0, z = 0;
+            Console.WriteLine("Array: ");
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = rnd.Next(-100, 101);
+                Console.Write(arr[i] + (i == N - 1 ? "\n" : ", "));
+                if (arr[i] == 0) c++;
+                if (c == 1) z = i;
+                if (Math.Abs(arr[i]) < min1) { min1 = Math.Abs(arr[i]); l = i; }
+                if (Math.Abs(arr[i]) > max) { max = Math.Abs(arr[i]); g = i; } 
+                if (arr[i] < min) { min = arr[i]; k = i; }
+            }
+            if (c > 1)  for (; z < c; z++) { rez2 += arr[z]; }
+            if (k == N - 1) Console.WriteLine("Добуток елементів, що розташовані після мінімального елементу " + arr[k] + ": 0");
+                    else 
+            {
+                for (int i = k + 1; i < N; i++) { rez *= arr[i]; }
+                        Console.WriteLine("Добуток елементів, що розташовані після мінімального елементу " + arr[k] + ": " + rez);
+            }
+            for (int i = 0; i < N; i++)
+            {
+                if (arr[i] < 0) n++;
+                if (n == 1) a = i;
+                if (arr[i] > 0) m++;
+                if (m == 2) b = i;
+            }
+            if (a > b) b++; { for (; b < a; b++) {rez1 += arr[b];} }
+            if (b > a) { for (; a < b; a++) {rez1 += arr[a];} }
+            if (l > g) for (g = g + 1; g < l; g++) { rez3 *= arr[g]; }
+            if (l < g) for (l = l + 1; g > l; l++) { rez3 *= arr[l]; }
+
+            Console.WriteLine("Cума елементів масиву, що розташовані між першим останнім нульовими елементами: " + rez2);    
+            Console.WriteLine("Cума елементів масиву, що розташовані між першим від'ємним та другим додатним елементами: " + rez1);
+            Console.WriteLine("Добуток елементів масиву, що розташовані між найбільшим та найменшим за модулями елементами: " + rez3);
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Введите N: ");
+            int N = int.Parse(Console.ReadLine());
+            double[] arr = new double[N];
+            Random rnd = new Random();
+            double sum = 0;
+            int ind2 = 0;
+            double sum1 = 0, ind = 0, max = 0, maxm = 0;     
+            Console.WriteLine("Array: ");
+                   
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = (101 + 100)*rnd.NextDouble() - 100;
+                Console.Write(arr[i] + (i == N - 1 ? "\n" : ", "));
+                if (arr[i] < 0)
+                    sum += arr[i];
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                    ind = i;
+                }
+                if (Math.Abs(arr[i]) > maxm)
+                {
+                    maxm = Math.Abs(arr[i]);
+                    ind2 = i;
+                }
+                if (arr[i] > 0)
+                    sum1 += i;
+            }
+            Console.WriteLine("max = arr["+ ind +"] = " + max);           
+            Console.WriteLine("Сума від'ємних елементів дорівнює " + sum);
+            Console.WriteLine("Максимальним за модулем є елемент "+ arr[ind2]);
+            Console.WriteLine("Cума індексів додатніх елементів дорівнює "+ sum1);
+            Console.ReadKey();
+                   
+        }
+
+    }
+}
